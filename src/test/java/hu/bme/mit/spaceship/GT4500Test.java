ee560f322ed953e1666b9a06da41ed2fdd4dd9f2
TheFlyingPiano99 <simonzolika1999@gmail.com>
2022-05-06 11:43:51 +0200

    JaCoCo implemented

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -1,4 +1,4 @@
-package hu.bme.mit.spaceship;
+ package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
@@ -47,4 +47,123 @@ public class GT4500Test {
     verify(mockTSSecondary, times(1)).fire(1);
   }
 
+  @Test
+  public void fire_single_twice_success() {
+    // Arrange
+
+    // Act
+    boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result1);
+    assertEquals(true, result2);
+    verify(mockTSPrimary, times(1)).fire(1);
+    verify(mockTSSecondary, times(1)).fire(1);
+  } 
+  @Test
+  public void fire_single_twice_first_failure() {
+    // Arrange
+    when(mockTSPrimary.fire(1)).thenReturn(false);
+
+    // Act
+    boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result1);
+    assertEquals(true, result2);
+    verify(mockTSPrimary, times(1)).fire(1);
+    verify(mockTSSecondary, times(1)).fire(1);
+  }
+
+  @Test
+  public void fire_single_twice_second_failure() {
+    // Arrange
+    when(mockTSSecondary.fire(1)).thenReturn(false);
+
+    // Act
+    boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result1);
+    assertEquals(false, result2);
+    verify(mockTSPrimary, times(1)).fire(1);
+    verify(mockTSSecondary, times(1)).fire(1);
+  }
+
+  @Test
+  public void fire_single_twice_first_out_of_ammo() {
+    // Arrange
+    when(mockTSPrimary.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result1);
+    assertEquals(true, result2);
+    verify(mockTSPrimary, times(0)).fire(1);
+    verify(mockTSSecondary, times(2)).fire(1);
+  }
+
+  @Test
+  public void fire_single_twice_second_out_of_ammo() {
+    // Arrange
+    when(mockTSSecondary.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result1);
+    assertEquals(true, result2);
+    verify(mockTSPrimary, times(2)).fire(1);
+    verify(mockTSSecondary, times(0)).fire(1);
+  }
+
+  @Test
+  public void fire_single_twice_both_out_of_ammo() {
+    // Arrange
+    when(mockTSPrimary.isEmpty()).thenReturn(true);
+    when(mockTSSecondary.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result1);
+    assertEquals(false, result2);
+    verify(mockTSPrimary, times(0)).fire(1);
+    verify(mockTSSecondary, times(0)).fire(1);
+  }
+
+  @Test
+  public void fire_all_twice() {
+    // Arrange
+
+    // Act
+    boolean result1 = ship.fireTorpedo(FiringMode.ALL);
+    boolean result2 = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(true, result1);
+    assertEquals(true, result2);
+    verify(mockTSPrimary, times(2)).fire(1);
+    verify(mockTSSecondary, times(2)).fire(1);
+  }
+
+  @Test
+  public void fire_laser() {
+
+    Boolean result = ship.fireLaser(FiringMode.SINGLE);
+
+    assertEquals(false, result);
+  }
+
 }
+
